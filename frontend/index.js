60e606a33a33624dc9ea0e129cc2731de03df45d
Christian Lisle <lisleachristian@gmail.com>
2021-04-10 13:19:13 -0500

    Updated request url

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,5 @@
 //const serverURL = "https://8256c7bf-2e3d-4402-9260-69d9f4ec1717.mock.pstmn.io";//Change me to the localhost/springboot server address
-const serverURL = "http://127.0.0.1:8080";
+const serverURL = "127.0.0.1:8080";
 
 function submit()
 {
