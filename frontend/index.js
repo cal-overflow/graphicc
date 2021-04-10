f46e87b7f5ab276ea5ae1466deadb705fd1b1c9e
carterjb <carterjb@iastate.edu>
2021-04-09 22:01:01 -0500

    working rest api for frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,24 @@
 function submit()
 {
-  //Submit button pressed. Do something
+  postRequest("https://8256c7bf-2e3d-4402-9260-69d9f4ec1717.mock.pstmn.io/test");
+}
+function getRequest(url)
+{
+  fetch(url,{
+    method: "GET"
+  }).then(
+    response => response.text()
+  ).then(
+    html => console.log(html)
+  );
+}
+function postRequest(url)
+{
+  fetch(url,{
+    method: "POST"
+  }).then(
+    response => response.text()
+  ).then(
+    html => console.log(html)
+  );
 }
