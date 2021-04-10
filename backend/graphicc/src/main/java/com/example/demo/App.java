df03439d613a76883d6d6c2e3ab9aff39f0f14c1
CarterTheMan <cartermoseley20@gmail.com>
2021-04-09 22:07:02 -0500

    Added Graphicc class (still needs the equation for each part) and added the GraphiccRepository class

---
+++ backend/graphicc/src/main/java/com/example/demo/App.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+	
+	
+
+}
