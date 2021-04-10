e9d9066654ddc077c369ee9450ed7c4c951a3623
Christian Lisle <lisleachristian@gmail.com>
2021-04-10 03:00:36 -0500

    heroku

--- frontend/server.js
+++ frontend/server.js
@@ -1,4 +1,15 @@
 const connect = require('connect');
 var static = require('serve-static');
 const PORT = process.env.PORT || 3000;
+const fs = require('fs');
+
+fs.readdir(directory, (err, files) => {
+    files.forEach(file => {
+        console.log(file);
+    });
+});
+
 connect().use(static('views/')).listen(PORT, () => console.log('Server running on port ${PORT}'));
+
+/*const PORT = process.env.PORT || 3000;
+require('http').createServer(require('serve-static')('.')).listen(PORT);*/
