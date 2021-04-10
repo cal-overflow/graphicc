51d1da5cb69758e4859cd76d90a3db18bb61ee87
CarterTheMan <cartermoseley20@gmail.com>
2021-04-10 03:07:57 -0500

    save backend

---
+++ backend/Graphicc/src/test/java/Graphicc/GraphiccApplicationTests.java
@@ -0,0 +1,13 @@
+package Graphicc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphiccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
