70057b6e3b5a54357ebd0a7bc4e743b8f12a6925
Christian Lisle <lisleachristian@gmail.com>
2021-04-10 02:45:25 -0500

    Updated port

--- frontend/server.js
+++ frontend/server.js
@@ -1,4 +1,4 @@
 const connect = require('connect');
 var static = require('serve-static');
-
-connect().use(static('views/')).listen(8080, () => console.log('Server running on port 8080'));
+const PORT = process.env.PORT || 3000;
+connect().use(static('views/')).listen(PORT, () => console.log('Server running on port ${PORT}'));
